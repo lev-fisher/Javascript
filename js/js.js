e48fd308152d846e9414a70634e0437c5bde555f
linaro <linaro@lev.lake>
2016-10-14 07:47:05 +0000

    added js files

---
+++ js/js.js
@@ -0,0 +1,152 @@
+/* CHAPTER 1 CALCULATOR */
+/*
+var a=Number(prompt('choose first number'));
+var b=Number(prompt('choose second number'));
+
+var op=prompt('choose operation \"+\", \"-\", \"*\, \"/\"')
+
+switch (op) {
+	case '+':
+	alert('a + b = ' + (a+b));
+	break;
+	
+case '-':
+	alert('a - b = ' + (a-b));
+	break;
+	
+case '*':
+	alert('a * b = ' + (a*b));
+	break;
+	
+case '/':
+	alert('a / b = ' + (a/b));
+	break;
+} 
+*/
+
+
+/* TEST FOR CICLES */
+
+/*for (var i=0; i<1001; i+=2) {
+	console.log(i);
+}*/
+
+/*for (var i=1; i<1001; i+=2) {
+	console.log(i);
+}*/
+
+/*var x=0
+while (x<=1000) {console.log(x); x++}*/
+
+/*var x=0
+do {
+	console.log(x); x++
+} while(x<=1000);*/
+
+/*var x=0
+while (x<=1000) {console.log(x); x+=2}*/
+
+/*var x=1
+while (x<=1000) {console.log(x); x+=2}*/
+
+/*var x=0
+do {
+	console.log(x); x+=2;
+} while(x<=1000)*/
+
+
+
+/* CHAPTER 2 FUNCTIONS */
+
+/*var a=5;
+
+function foo() {
+	var b=3;
+	console.log(a);
+	console.log(b);
+}
+
+foo();*/
+
+/*var foo=function(name) {
+	console.log('Hello ' + name);
+};
+
+foo('Lev')*/
+
+/*var sum=function(a, b) {
+	return a+b;
+};
+console.log(sum(5, 3));
+
+var min=function (a, b) {
+	return a-b;
+};
+console.log(min(5, 3))
+
+var mn=function(a, b) {
+	return a*b;
+};
+console.log(mn(5, 3))
+
+*/
+
+/*var arr = [3, 5 ,4];
+var i=0;
+console.log(arr[i])*/
+
+
+/*arr=[1, 4, 8, 4, 7, 2,9]
+arr.sort()
+arr.reverse()
+CONSOLE ONLY*/
+
+
+/* TEST FUNCTIONS */
+
+//#1
+/*var foo=function(name) {
+	console.log('Hello ' + name)
+}
+
+foo('Lev')*/
+
+//#2
+/*var sum=function(a, b) {
+	return a+b;
+};
+console.log(sum(5, 3));*/
+
+/*var sum=function() {
+	var sum=0
+	for (var i=0; i<arguments.length; i++){
+		sum += arguments[i];
+	}
+	
+	return sum
+};
+
+console.log(sum(2, 4, 3))
+*/
+
+//#3
+function min() {
+	var min=argument[];
+	for(var i=0; i<arguments.length)
+	
+	
+}
+
+	 
+
+
+//#4
+
+//Notates
+	//Math.floor(Math.random()*1000)
+	//only console
+
+
+
+
+
